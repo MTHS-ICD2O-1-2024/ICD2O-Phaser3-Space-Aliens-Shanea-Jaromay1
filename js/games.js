d09facc1fb0a3d54d99cf9c418a2895a76772223
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-11 17:04:07 +0000

    init

---
+++ js/games.js
@@ -0,0 +1,31 @@
+/* global Phaser */
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
+//
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
