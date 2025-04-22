ef820cc82c9d0a60b92fa022ba0b23709d1339f4
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-22 16:59:34 +0000

    init

--- js/game.js
+++ js/game.js
@@ -4,29 +4,41 @@
 //
 // Created by: Shanea Jaromay
 // Created on: Apr 2025
-// This is the PHASER3 game configuration file
+// This is the Phaser3 game configuration file
+
+import SplashScene from "./splashScene.js"
+
+//* Game scene */
+const splashScene = new SplashScene()
 
 /**
- * Start Phaser Game.
- */
+* Start Phaser Game.
+*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
   // set background color
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
-
 const game = new Phaser.Game(config)
-console.log(game)
+// console.log(game)
+
+
+// load scenes
+// Note: remember any "key" is global and CANNOT be reused
+game.scene.add("splashScene", splashScene)
+
+// the start scene
+game.scene.start("splashScene")
---
+++ js/splashScene.js
@@ -0,0 +1,57 @@
+/* global Phaser */
+
+// Copyright (c) 2025 Shanea JaromayAll rights reserved
+//
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
+// This is the Phaser3 game configuration file
+
+/**
+* This class is the Splash Scene.
+*/
+class SplashScene extends Phaser.Scene {
+  /**
+    * This method is the constructor.
+    */
+  constructor() {
+    super({ key: "splashScene" })
+  }
+
+  /**
+  * Can be defined on your own Scenes.
+  * This method is called by the Scene Manager when the scene starts,
+  *  before preload() and create().
+  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+  */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+  * Can be defined on your own Scenes.
+  * Use it to load assets.
+  */
+  preload() {
+    console.log("SplashScene")
+  }
+
+  /**
+  * Can be defined on your own Scenes.
+  * Use it to create your game objects.
+  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+  */
+  create(data) {
+    //pass
+  }
+
+  /**
+  * Should be overridden by your own Scenes.
+  * This method is called once per game step while the scene is running.
+  *  @param {number} time - The current time.
+  *  @param {number} - The delta time in ms since the last frame.
+  */
+  update(time, delta) {
+    // pass
+  }
+}
+export default SplashScene
