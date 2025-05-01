9a6b769b045f364815bb2e8f49c72cd09b77a78c
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-01 16:41:00 +0000

    please work

--- js/menuScene.js
+++ js/menuScene.js
@@ -36,8 +36,8 @@ class MenuScene extends Phaser.Scene {
    */
   preload() {
     console.log("Menu Scene")
-    this.preload.image('menuSceneBackground', './assets/aliens_screen_image2.jpg')
-    this.preload.image('startButton', './assets/start.png')
+    this.load.image('menuSceneBackground', './assets/aliens_screen_image2.jpg')
+    this.load.image('startButton', './assets/start.png')
   }
 
   /**
@@ -50,7 +50,7 @@ class MenuScene extends Phaser.Scene {
     this.menuSceneBackgroundImage.x = 1920 / 2
     this.menuSceneBackgroundImage.y = 1080 / 2
 
-    this.startButton = this.add.prite(1920 / 2, 1080 / 2 + 100, 'startButton')
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, 'startButton')
     this.startButton.setInteractive({ useHandCursor: true })
     this.startButton.on('pointerdown', () => this.clickButton())
   }
