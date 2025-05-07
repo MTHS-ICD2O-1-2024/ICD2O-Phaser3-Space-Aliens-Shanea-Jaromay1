4b25bd97a7a322bb1737a18a5554c5567af27f0a
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-07 16:32:56 +0000

    init

--- js/gameScene.js
+++ js/gameScene.js
@@ -15,6 +15,9 @@ class GameScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "gameScene" })
+
+    this.background = null
+    this.ship = null
   }
 
   /**
@@ -33,6 +36,10 @@ class GameScene extends Phaser.Scene {
    */
   preload() {
     console.log("Game Scene")
+
+    // image
+    this.load.image('startBackground', './assets/starBackground.png')
+    this.load.image('ship', './assets/spaceShip.png')
   }
 
   /**
@@ -41,6 +48,10 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
+    this.background = this.add.image(0, 0, 'startBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
     // pass
   }
 
